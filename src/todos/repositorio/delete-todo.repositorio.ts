52b95ebfefc45de1a4be3388580c2bf9a914b7b6
ThamyresS2 <thamyrestlms@gmail.com>
2026-03-11 11:56:02 -0400

    oi

--- src/todos/repositorio/delete-todo.repositorio.ts
+++ src/todos/repositorio/delete-todo.repositorio.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "prisma.databases";
-import { CreateTodoDto } from "../dto/create-todo.dto";
-//  
-@Injectable()
-export class DeleteTodoRepository{
-    constructor (private readonly prisma: PrismaService){}
 
-    async delete (data: DeleteTodoDto){
-        return await this.prisma.todo.delete({data})
-    }
+@Injectable()
+export class DeleteTodoRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async delete(data: { id: string }) {
+    return await this.prisma.todo.delete({
+      where: data
+    });
+  }
 }
