af43ae0aba6f0ab21c7d9f55e1604b6dbfed5bd0
ThamyresS2 <thamyrestlms@gmail.com>
2026-03-18 12:14:28 -0400

    13/03/2026

---
+++ src/todos/use-cases/create-todo.use-case.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable, Logger } from "@nestjs/common";
+import { CreateTodoRepository } from "../repositorio";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+
+@Injectable()
+export class CreateTodoUseCase {
+    constructor(
+        private readonly createTodoRepository: CreateTodoRepository,
+        private readonly logger: Logger,
+    ) { }
+
+    async execute(data: CreateTodoDto) {
+        try {
+            this.logger.log('Creating toDo...');
+            const todo = await this.createTodoRepository.create(data);
+            this.logger.log('ToDo created sucessfully');
+            return todo;
+        }catch (error){
+                this.logger.error(error);
+                throw new Error('Failed to create toDo');
+            };
+        }
+}
