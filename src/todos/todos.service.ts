145ad451adbf5874e4d4f8d028a2eedbad0411a2
ThamyresS2 <thamyrestlms@gmail.com>
2026-05-06 15:59:45 -0300

    AUTH

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { CreateTodoRepository, FindAllTodoRepository } from './repositorio';
+import { find } from 'rxjs';
+import { create } from 'node:domain';
 
 @Injectable()
 export class TodosService {
-  create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+  constructor(
+    private readonly findAllTodoRepository: FindAllTodoRepository,
+    private readonly createTodoRepository: CreateTodoRepository,
+    private readonly findOneTodoRepository: FindAllTodoRepository,
+    private readonly updateTodoRepository: CreateTodoRepository,
+    private readonly removeTodoRepository: CreateTodoRepository,
+       
+  ){}
+
+  create(data: CreateTodoDto) {
+    return this.createTodoRepository.create(data);
   }
 
-  findAll() {
-    return `This action returns all todos`;
+  findAll(findAllTodoDto) {
+    return this.findAllTodoRepository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  findOne(findOneTodoDto) {
+    return this.findOneTodoRepository.findOne();
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+  update(UpdateTodoDto: number, updateTodoDto: UpdateTodoDto) {
+    return this.updateTodoRepository.update(updateTodoDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} todo`;
+    return this.removeTodoRepository.remove(id);
   }
 }
